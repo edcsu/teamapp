c59441451b6b56ca08bcafcc4cbfbe6b45e98919
Ssewannonda Keith Edwin <skeith696@gmail.com>
2021-12-20 18:58:20 +0300

    feat: add team generator functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,8 +9,12 @@ import {FormControl, Validators} from '@angular/forms';
 export class AppComponent {
   title = 'teamapp';
   isNameEmpty:boolean = false;
+  isTeamEmpty:boolean = false;
+  isTeamLess:boolean = false;
   competitor = '';
   competitors: string[] = [];
+  numberOfTeams:number | "" = "";
+  teams:string [][] = [];
 
   addCompetitor(){
     if (!this.competitor) {
@@ -26,5 +30,44 @@ export class AppComponent {
     this.competitor = newCompetitor;
   }
 
+  onNumberOfTeamsInput(teamNumber: string){
+    this.numberOfTeams = Number(teamNumber);
+  }
+
+  generateTeams(){
+    if (!this.numberOfTeams || this.numberOfTeams <= 0) {
+      this.isTeamEmpty = true;
+      return;
+    }
+    
+    if (this.competitors.length < this.numberOfTeams) {
+      this.isTeamLess = true;
+      return;
+    }
+
+    const allCompetitors = [...this.competitors];
+
+    while (allCompetitors.length) {
+      for (let i = 0; i < this.numberOfTeams; i++) {
+        const randomIndex = Math.floor(Math.random() * allCompetitors.length);
+        const chosen = allCompetitors.splice(randomIndex, 1)[0];
+        if(!chosen) break;
+        if (this.teams[i]) {
+          this.teams[i].push(chosen);
+        } else {
+          this.teams[i] = [chosen];
+        }   
+      }
+    }
+
+    this.isNameEmpty = false;
+    this.isTeamEmpty = false; 
+    this.isTeamLess = false; 
+
+    this.competitors = [];
+    this.numberOfTeams = "";
+  }
+
   inputFormControl = new FormControl('', [Validators.required, Validators.nullValidator]);
+  teamFormControl = new FormControl('', [Validators.required, Validators.nullValidator]);
 }
